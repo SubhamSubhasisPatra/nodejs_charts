d4a38b294c56db08119da47c079cd44c9701b8c5
Subhasis <subhasispatra94@gmail.com>
2021-08-31 08:32:19 +0530

    PM2 is added for minitoring the process

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const cluster = require("cluster");
-const totalCPUs = require('os').cpus().length;
+// const totalCPUs = require('os').cpus().length;
 
 const barchChart = require("./barchart_3d")
 
+/*
 if (cluster.isMaster) {
     
     console.log(`Total Number of CPU Counts is ${totalCPUs}`);
@@ -34,3 +35,17 @@ else {
 
     app.listen(3000, () => console.log("Express App is running on PORT : 3000"));
 }
+*/
+
+const app = express();
+    app.get("/", async (request, response) => {
+        try {
+            console.log(`Worker Process Id - ${cluster.worker.process.pid} has accepted the request!`);
+            let svg_data = await barchChart.generateBarChart3D();
+            response.send(`<h1>${svg_data}</h1>`);
+        } catch (error) {
+            console.log(error);
+        }
+    });
+
+    app.listen(3000, () => console.log("Express App is running on PORT : 3000"));
